a69dbb502640caf75e5ec899b4b373677fa29c15
CourtimusPrime <dashdale02@gmail.com>
2025-06-28 12:47:01 +0400

    Added Tailwind support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
